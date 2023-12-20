393b7f1bf8fbba746ad762bf5b5e045b8550db33
antonbaider <email.b.anton@gmail.com>
2023-12-20 20:28:25 +0100

    done

--- src/main/java/jom/com/softserve/s5/task2/App.java
+++ src/main/java/jom/com/softserve/s5/task2/App.java
@@ -1,10 +1,15 @@
 package jom.com.softserve.s5.task2;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class App {
-    public static double[] getChanged(
-            double[] initialArray
-            /*place the second parameter here*/    ) {
-    	//your code
-    	return null;
+    public static Consumer<double[]> cons = array ->
+            Arrays.setAll(array, i -> array[i] > 2 ? array[i] * 0.8 : array[i] * 0.9);
+
+    public static double[] getChanged(double[] initialArray, Consumer<double[]> cons) {
+        double[] newArray = initialArray.clone();
+        cons.accept(newArray);
+        return newArray;
     }
 }
