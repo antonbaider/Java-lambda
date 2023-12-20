a478c38ec32728b11a92d13a9c6d85ae0cfd480c
antonbaider <email.b.anton@gmail.com>
2023-12-20 20:58:44 +0100

    done

--- src/main/java/jom/com/softserve/s5/task3/App.java
+++ src/main/java/jom/com/softserve/s5/task3/App.java
@@ -1,13 +1,14 @@
 package jom.com.softserve.s5.task3;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
 
 public class App {
 
-	// Write your code here
+    public static BinaryOperator<String> greetingOperator = (name, surname) -> String.format("Hello %s %s!!!", name, surname);
 
-	public static List<String> createGreetings(List<Person> people /* Second parameter here */) {
-		// Write your code here
-		return null;
-	}
+    public static List<String> createGreetings(List<Person> people, BinaryOperator<String> greetingOperator) {
+        return people.stream().map(person -> greetingOperator.apply(person.name, person.surname)).toList();
+    }
 }
