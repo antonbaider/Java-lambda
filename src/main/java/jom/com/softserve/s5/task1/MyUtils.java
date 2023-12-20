0b301e7b52273c74dc6c4f902703ac7de7a75fd9
antonbaider <email.b.anton@gmail.com>
2023-12-20 18:44:53 +0100

    done

--- src/main/java/jom/com/softserve/s5/task1/MyUtils.java
+++ src/main/java/jom/com/softserve/s5/task1/MyUtils.java
@@ -1,8 +1,8 @@
-package jom.com.softserve.s5.task1;
-
+import java.util.Arrays;
+import java.util.function.Predicate;
 public class MyUtils {
-    public static int getCount( /* Place the parameters here */ ) {
-    	// Write your code here
-        return 0;
+
+    public static int getCount(int[] arr, Predicate<Integer> predicate) {
+        return (int) Arrays.stream(arr).boxed().filter(predicate).count();
     }
 }
