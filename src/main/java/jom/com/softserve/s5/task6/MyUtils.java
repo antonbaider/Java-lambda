d7ad4b3b91ac21e0a90eb32f10edfc26f677b9ca
antonbaider <email.b.anton@gmail.com>
2023-12-21 00:45:58 +0100

    done

--- src/main/java/jom/com/softserve/s5/task6/MyUtils.java
+++ src/main/java/jom/com/softserve/s5/task6/MyUtils.java
@@ -1,27 +1,24 @@
-package jom.com.softserve.s5.task6;
-
 import java.util.function.Predicate;
 import java.util.function.BiFunction;
 import java.util.ArrayList;
 import java.util.List;
 
-class MyUtils {
-	public static int findMaxByCondition(List<Integer> numbers, Predicate<Integer> pr) {
-		 // Write your code here
-		return 0;
-	}
+public class MyUtils {
+
+    public static int findMaxByCondition(List<Integer> numbers, Predicate<Integer> predicate) {
+        return numbers.stream().filter(predicate).mapToInt(Integer::intValue).max().orElse(0);
+    }
 }
 
 class User {
-	public final List<Integer> values = new ArrayList<Integer>();
 
-	int getFilterdValue(/* arguments */) {
-		 // Write your code here
-		return 0;
-	}
+    public final List<Integer> values = new ArrayList<>();
 
-	int getMaxValueByCondition(Predicate<Integer> predicate) {
-		 // Write your code here
-		return 0;
-	}
+    int getFilterdValue(BiFunction<List<Integer>, Predicate<Integer>, Integer> filterFunction, Predicate<Integer> predicate) {
+        return filterFunction.apply(values, predicate);
+    }
+
+    int getMaxValueByCondition(Predicate<Integer> predicate) {
+        return getFilterdValue(MyUtils::findMaxByCondition, predicate);
+    }
 }
